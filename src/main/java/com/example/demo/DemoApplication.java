eee0cd822c18348181e0bda049e09aef62c7ef3a
Eric Espinoza <espinoza.eric@gmail.com>
2024-06-28 01:34:19 -0400

    configurando loggers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		logger.debug("Mi mensaje debug");				// este quedan oculto el mensage debug
+		logger.info("Mi mensaje info");
+		logger.warn("Mi mensaje warning");
+		logger.error("Mi mensaje error");
+		//logger.trace("Mi mensaje trace");
+
 	}
 
 }
